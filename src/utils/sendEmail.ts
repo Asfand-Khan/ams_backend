1707212de47a997aef5dbd144b29bf2c649a0f9a
Asfand-Khan <khanasfand174@gmail.com>
2025-07-31 13:16:37 +0500

    fixes

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -16,14 +16,13 @@ export const sendEmail = async (options: SendEmailOptions) => {
   const transporter: Transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT) || 465,
-    secure: true,
+    secure: process.env.SMTP_PORT === "465",
     auth: {
       user: process.env.SMTP_USER, // use SMTP_USER for AWS SES
       pass: process.env.SMTP_PASS,
     },
-    tls: {
-      rejectUnauthorized: false,
-    },
+    logger: true,
+    debug: true,
   } as SMTPTransport.Options);
 
   // Optional: Verify connection before sending
