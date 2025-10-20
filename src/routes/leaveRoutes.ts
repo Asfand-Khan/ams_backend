6bc3a5c749e5c35969e80cf7d7c2a597cd7a648e
Ammar Ali <rajaammarali2003@gmail.com>
2025-10-20 17:20:01 +0500

    commit attendee list

--- src/routes/leaveRoutes.ts
+++ src/routes/leaveRoutes.ts
@@ -6,10 +6,11 @@ import {
   getSingleLeaveHandler,
   leaveSummaryHandler,
 } from "../controllers/leaveController";
+import { authenticate } from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.post("/all", getAllLeavesHandler);
+router.post("/all", authenticate,getAllLeavesHandler);
 router.post("/", createLeaveHandler);
 router.post("/single", getSingleLeaveHandler);
 router.post("/summary", leaveSummaryHandler);
